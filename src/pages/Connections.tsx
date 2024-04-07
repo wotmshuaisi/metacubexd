d9d9a77c0f3873916a1fc1599a9ee21b4e842dba
WANGSHIFU <16408269+wotmshuaisi@users.noreply.github.com>
2024-04-07 19:46:10 +0100

    feat: add geoip location flag to the host column
    Signed-off-by: WANGSHIFU <16408269+wotmshuaisi@users.noreply.github.com>

--- src/pages/Connections.tsx
+++ src/pages/Connections.tsx
@@ -38,7 +38,7 @@ import {
   on,
 } from 'solid-js'
 import { twMerge } from 'tailwind-merge'
-import { closeAllConnectionsAPI, closeSingleConnectionAPI } from '~/apis'
+import { closeAllConnectionsAPI, closeSingleConnectionAPI, fetchGeoIPAPI } from '~/apis'
 import {
   Button,
   ConnectionsSettingsModal,
@@ -310,6 +310,15 @@ export default () => {
     getCoreRowModel: getCoreRowModel(),
   })
 
+  const validCountryCodes = ["af","al","dz","as","ad","ao","ai","aq","ag","ar","am","aw","au","at","az","bs","bh","bd","bb","by","be","bz","bj","bm","bt","bo","bq","ba","bw","bv","br","io","bn","bg","bf","bi","cv","kh","cm","ca","ky","cf","td","cl","cn","cx","cc","co","km","cd","cg","ck","cr","hr","cu","cw","cy","cz","ci","dk","dj","dm","do","ec","eg","sv","gq","er","ee","sz","et","fk","fo","fj","fi","fr","gf","pf","tf","ga","gm","ge","de","gh","gi","gr","gl","gd","gp","gu","gt","gg","gn","gw","gy","ht","hm","va","hn","hk","hu","is","in","id","ir","iq","ie","im","il","it","jm","jp","je","jo","kz","ke","ki","kp","kr","kw","kg","la","lv","lb","ls","lr","ly","li","lt","lu","mo","mg","mw","my","mv","ml","mt","mh","mq","mr","mu","yt","mx","fm","md","mc","mn","me","ms","ma","mz","mm","na","nr","np","nl","nc","nz","ni","ne","ng","nu","nf","mp","no","om","pk","pw","ps","pa","pg","py","pe","ph","pn","pl","pt","pr","qa","mk","ro","ru","rw","re","bl","sh","kn","lc","mf","pm","vc","ws","sm","st","sa","sn","rs","sc","sl","sg","sx","sk","si","sb","so","za","gs","ss","es","lk","sd","sr","sj","se","ch","sy","tw","tj","tz","th","tl","tg","tk","to","tt","tn","tr","tm","tc","tv","ug","ua","ae","gb","um","us","uy","uz","vu","ve","vn","vg","vi","wf","eh","ye","zm","zw","ax",]
+  const getIPCode = (ip: string) => {
+    const countryCode = fetchGeoIPAPI(ip).code    
+    if (validCountryCodes.includes(countryCode)) {
+      return String.fromCodePoint(...[...countryCode.toUpperCase()].map(x=>0x1f1a5+x.charCodeAt())) + " "
+    }
+    return "[" + countryCode.toUpperCase() + "] "
+  }
+
   const sourceIPHeader = table
     .getFlatHeaders()
     .find(({ id }) => id === CONNECTIONS_TABLE_ACCESSOR_KEY.SourceIP)
@@ -525,10 +534,10 @@ export default () => {
                               cell.getContext(),
                             )
                           ) : cell.getIsPlaceholder() ? null : (
-                            flexRender(
-                              cell.column.columnDef.cell,
-                              cell.getContext(),
-                            )
+                            cell.column.id === "host"?
+                            getIPCode(cell.row.original.metadata.destinationIP)+flexRender(cell.column.columnDef.cell, cell.getContext())+`(${cell.row.original.metadata.destinationIP})`
+                            :
+                            flexRender(cell.column.columnDef.cell, cell.getContext())
                           )}
                         </td>
                       )
