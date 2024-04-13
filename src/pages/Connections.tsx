ef4495fb7fc8c9fb5f803f2fab5455f02173fb71
abc <16408269+wotmshuaisi@users.noreply.github.com>
2024-04-13 13:14:05 +0100

    Fix country code toUpperCase from undefineded

--- src/pages/Connections.tsx
+++ src/pages/Connections.tsx
@@ -316,7 +316,7 @@ export default () => {
     if (validCountryCodes.includes(countryCode)) {
       return String.fromCodePoint(...[...countryCode.toUpperCase()].map(x=>0x1f1a5+x.charCodeAt())) + " "
     }
-    return "[" + countryCode.toUpperCase() + "] "
+    return "[" + countryCode?countryCode.toUpperCase():"NULL" + "] "
   }
 
   const sourceIPHeader = table
